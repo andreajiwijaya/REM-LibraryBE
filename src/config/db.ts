3c11e7e2785e56ca55d1d2c7ba9b78938cab6242
Andre Ajiwijaya <12350111588@students.uin-suska.ac.id>
2025-06-12 00:25:14 +0700

    fix y

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,16 @@
+// src/config/db.ts
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export default prisma;
+const client = globalThis.prisma || new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], // INI BAGIAN PENTING
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = client;
+}
+
+export default client;
